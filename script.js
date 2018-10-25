61fbdb3f83ed77ecc81bfb14233f233c973d8cbe
AnneMarieHarm <amharm@earthlink.net>
2018-10-24 23:27:42 -0400

    Loops_and_Conditions_Homework_first_commit

---
+++ script.js
@@ -0,0 +1,64 @@
+"use strict"
+
+//Loops
+
+//Counting Loops
+
+// for loop
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+
+// while loop
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(i++);
+// }
+
+// i = 10;
+// while (i >= 0) {
+//     console.log(i--);
+// }
+
+//do while loop
+
+// let i = 0;
+// do {
+//     console.log(i++);
+// }
+// while (i <= 10);
+
+// i = 10;
+// do {
+//     console.log(i--);
+// }   
+// while (i >= 0);
+
+//for... of loop
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let number of numbers) {
+//     console.log(number);
+// }
+
+//Conditions
+
+//Heating/Cooling Exercise
+
+// let actualTemp = 76;
+// let desiredTemp = 68;
+
+// if (actualTemp < desiredTemp) {
+//     console.log("Run heat");
+// }else if (actualTemp > desiredTemp) {
+//     console.log("Run A/C");
+// }else if (actualTemp === desiredTemp) {
+//     console.log("Standby");
+// }
+
+
